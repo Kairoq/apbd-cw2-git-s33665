417ccdfb28ad5091374560ac6de3d96168ce4366
Kairoq <maksik771@wp.pl>
2026-03-23 23:14:13 +0100

    Added return info methods

--- ConsoleApp1/Service.cs
+++ ConsoleApp1/Service.cs
@@ -36,4 +36,20 @@ public class Service
         equipment.IsAvailable = true;
         return rental;
     }
+
+    public IEnumerable<Equipment> GetEquipment()
+    {
+        return _equipment;
+    }
+
+    public IEnumerable<Equipment> GetAvailableEquipment()
+    {
+        return _equipment.Where(e => e.IsAvailable);
+    }
+    
+    public IEnumerable<Rental> GetActiveRentalsForUser(User user)
+    {
+        return _rentals.Where(r => r.User == user && !r.IsReturned);
+    }
+
 }
